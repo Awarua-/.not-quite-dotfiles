73603a8d44d15f26c492f3480723287fa7b584aa
Dion Woolley <dwoolley@atlassian.com>
2018-02-19 09:25:30 +1100

    Add prepare commit message hook

--- zsh/custom.zsh
+++ zsh/custom.zsh
@@ -34,8 +34,9 @@ source $HOME/.not-quite-dotfiles/tmux/mux.sh
 
 export PATH="/usr/local/git/current/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:$(go env GOPATH)/bin"
 
-export NVM_DIR="/Users/noms/.nvm"
+export NVM_DIR="/Users/dwoolley/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
